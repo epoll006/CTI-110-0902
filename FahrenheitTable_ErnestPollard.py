d80f2c3c2531f1a31e122f52fbedd9c0fc4c15f1
epoll006 <43486136+epoll006@users.noreply.github.com>
2018-10-28 18:11:55 -0400

    Add files via upload

---
+++ FahrenheitTable_ErnestPollard.py
@@ -0,0 +1,9 @@
+#Celsius to Fahrenheit Table
+#10/11/2018
+#CTI-110 P4HW2
+#Ernest Pollard
+#
+for currentCelsiusTemperature in range( 21 ):
+    fahrenheitEquivalent = ( 9 / 5 ) * currentCelsiusTemperature + 32
+    print( currentCelsiusTemperature,"\t",fahrenheitTemperatureEquivalent )
+    
